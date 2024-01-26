49c65a16712978e9b9d6c245462c8480c377f1e8
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-01-26 23:43:49 +0600

    First Android development project

---
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.text_view_result);
+
+    }
+}
