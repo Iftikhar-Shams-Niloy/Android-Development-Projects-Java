09aa55414700037c54bb6ec8736335748d3c9100
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-02-04 01:12:38 +0600

    New app Developed

--- ShareCare/app/src/main/java/com/example/sharecare/MainActivity.java
+++ ShareCare/app/src/main/java/com/example/sharecare/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.sharecare;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     CardView startersCard;
@@ -15,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_main_course);
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
