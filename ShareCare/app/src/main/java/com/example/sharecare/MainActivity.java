69d47c798b08cf6d8e04fe0ebd7159c33bad178a
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-02-05 10:47:43 +0600

    App updated

--- ShareCare/app/src/main/java/com/example/sharecare/MainActivity.java
+++ ShareCare/app/src/main/java/com/example/sharecare/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     CardView startersCard;
     CardView mainsCard;
+    TextView emailAdress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_main_course);
-
         startersCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
-
                 startActivity(startersActivityIntent);
             }
         });
+        mainsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainsActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
+                startActivity(mainsActivityIntent);
+            }
+        });
+        TextView emailTextView = findViewById(R.id.text_view_email);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent EmailAppIntent = new Intent(Intent.ACTION_SENDTO);
+                EmailAppIntent.setData(Uri.parse("mailto:share.and.care5916@gmail.com")); // only email apps should handle this
+                startActivity(EmailAppIntent );
+
+            }
+        });
+
     }
 }
--- ShareCare/app/src/main/java/com/example/sharecare/MainCoursesActivity.java
+++ ShareCare/app/src/main/java/com/example/sharecare/MainCoursesActivity.java
@@ -1,6 +1,8 @@
 package com.example.sharecare;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,24 @@ public class MainCoursesActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_courses);
+
+        ListView mainCoursesList = findViewById(R.id.list_view_main_courses);
+
+        Dish[] mainCourses = {
+                new Dish("Chicken Biryani","Chicken Biryani sheraaa!",450),
+                new Dish("Morog Polao", "Morog Polao Shei tasty!!!", 420),
+                new Dish("Beef Teheri", "Ek bar khaile bar bar khaben!", 495),
+                new Dish("Grill and Nun", "Khete hebby taste, khawar por shonar Bangladesh", 380),
+                new Dish("Chicken Fried Rice", "Ken jaani, shobai khaite pochondo kore", 310),
+                new Dish("Prawn Fried Rice", "Better than Chicken FR but, no body cares to try",320),
+                new Dish("Beef Curry and Rice", "Bangali hoile apnar bhalo laagbei", 270),
+                new Dish("Mutton Curry and Rice", "Ekbar khaile, khaiben shoto-bar", 280),
+                new Dish("Dori Fish Fry", "Looks amazing, tastes even better", 290),
+                new Dish("Butter Chicken with Parathas", "Khaben ar shudhu mota hoben", 280)
+        };
+
+        ArrayAdapter<Dish> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mainCourses);
+
+        mainCoursesList.setAdapter(dishesAdapter);
     }
 }
