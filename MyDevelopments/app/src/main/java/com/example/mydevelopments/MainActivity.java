552695b861879a5371e807c3d67f54ea71ebf251
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-02-11 00:39:32 +0600

    *Project updated*

--- MyDevelopments/app/src/main/java/com/example/mydevelopments/MainActivity.java
+++ MyDevelopments/app/src/main/java/com/example/mydevelopments/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Project firstProject = new Project("First app", "This is my First app", 12345);
     }
 }
