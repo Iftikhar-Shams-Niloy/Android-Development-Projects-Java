e0e13fb92aedd3848eb5daf1212139c2fa343c17
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-02-14 01:49:16 +0600

    *Project Updated*

--- MyDevelopments/app/src/main/java/com/example/mydevelopments/MainActivity.java
+++ MyDevelopments/app/src/main/java/com/example/mydevelopments/MainActivity.java
@@ -19,7 +19,14 @@ public class MainActivity extends AppCompatActivity {
             new Development("BMI Calculator", "This is my Second app", R.drawable.bmi_app_logo),
             new Development("Converter Tool", "This is my Third app", R.drawable.converter_tools_logo),
             new Development("Take The Change", "This is my Fourth app", R.drawable.take_the_change_app),
-            new Development("Share and Care", "This is my Fifth app", R.drawable.ic_launcher_foreground),
+            new Development("Share and Care", "This is my Fifth app", R.drawable.share_and_care_logo),
+            new Development("The Dumb App", "This is my Sixth app", R.drawable.the_dumb_app_logo),
+            new Development("DUMMY", "DUMMY app", R.drawable.ic_launcher_foreground),
+            new Development("DUMMY", "DUMMY app", R.drawable.ic_launcher_foreground),
+            new Development("DUMMY", "DUMMY app", R.drawable.ic_launcher_foreground),
+            new Development("DUMMY", "DUMMY app", R.drawable.ic_launcher_foreground),
+            new Development("DUMMY", "DUMMY app", R.drawable.ic_launcher_foreground),
+            new Development("DUMMY", "DUMMY app", R.drawable.ic_launcher_foreground),
         };
 
         DevelopmentsAdapter adapter = new DevelopmentsAdapter(developments);
