5c1f2202dc981c878d428d26f19be5f987fe9ba4
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-02-17 12:41:14 +0600

    *PROJECT UPDATED*

--- MyDevelopments/app/src/main/java/com/example/mydevelopments/MainActivity.java
+++ MyDevelopments/app/src/main/java/com/example/mydevelopments/MainActivity.java
@@ -12,9 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView list = findViewById(R.id.recycler_view_projects);
 
-        Development[] developments = {
+
+    }
+
+
+}RecyclerView list = findViewById(R.id.recycler_view_projects);
+
+    Development[] developments = {
             new Development("First app", "This is my First app", R.drawable.ic_launcher_background),
             new Development("BMI Calculator", "This is my Second app", R.drawable.bmi_app_logo),
             new Development("Converter Tool", "This is my Third app", R.drawable.converter_tools_logo),
@@ -27,13 +32,8 @@ public class MainActivity extends AppCompatActivity {
             new Development("DUMMY", "DUMMY app", R.drawable.ic_launcher_foreground),
             new Development("DUMMY", "DUMMY app", R.drawable.ic_launcher_foreground),
             new Development("DUMMY", "DUMMY app", R.drawable.ic_launcher_foreground),
-        };
+    };
 
-        DevelopmentsAdapter adapter = new DevelopmentsAdapter(developments);
+    DevelopmentsAdapter adapter = new DevelopmentsAdapter(developments);
 
         list.setAdapter(adapter);
-
-    }
-
-
-}
