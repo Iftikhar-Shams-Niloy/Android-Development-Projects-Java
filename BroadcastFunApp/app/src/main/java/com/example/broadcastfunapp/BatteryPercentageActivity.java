f5172b4d90ab3deb60674ebbdcb27228561863d9
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2024-02-21 02:15:55 +0600

    *Project Updated*

--- BroadcastFunApp/app/src/main/java/com/example/broadcastfunapp/BatteryPercentageActivity.java
+++ BroadcastFunApp/app/src/main/java/com/example/broadcastfunapp/BatteryPercentageActivity.java
@@ -2,13 +2,37 @@ package com.example.broadcastfunapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
 
 public class BatteryPercentageActivity extends AppCompatActivity {
 
+    TextInputEditText editTextBatteryGuess;
+    Button buttonNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battery_percentage);
+
+        editTextBatteryGuess = findViewById(R.id.edit_text_battery_percentage);
+
+        buttonNext = findViewById(R.id.button_battery);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String batteryPercentage = Objects.requireNonNull(editTextBatteryGuess.getText()).toString(); // GETTING THE TEXT
+                Intent myIntent = new Intent(BatteryPercentageActivity.this, BatteryPercentageActivity2.class);
+                myIntent.putExtra("batteryPercentage",batteryPercentage); // SENDING THE TEXT
+                startActivity(myIntent);
+            }
+        });
+
+
     }
 }
